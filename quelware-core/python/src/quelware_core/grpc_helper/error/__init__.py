9efd4d76cc1313ac7bc477ce5f75fcc4f57553e0
Ryosuke IMAI <ryosuke@imaimaimai.net>
2026-02-23 00:58:54 +0900

    core: add helper functions for error propagation

---
+++ quelware-core/python/src/quelware_core/grpc_helper/error/__init__.py
@@ -0,0 +1,15 @@
+import json
+from google.rpc.error_details_pb2 import ErrorInfo
+def build_details(obj):
+    obj_json=json.dumps(obj)
+    return [ErrorInfo(metadata={"json":obj_json})]
+
+def extract_obj(details):
+    if details:
+        for detail in details:
+            if isinstance(detail, ErrorInfo):
+                obj_json = detail.metadata["json"]
+                obj = json.loads(obj_json)
+                return obj
+
+__all__ = ["build_details", "extract_obj"]
